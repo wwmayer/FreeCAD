c2cdd1c7a9d125d17a43d7fc16ef834852a84260
wmayer <wmayer@users.sourceforge.net>
2021-05-04 09:26:43 +0200

    Main: C++11: modernize redundant void-arg

--- src/Main/MainGui.cpp
+++ src/Main/MainGui.cpp
@@ -57,7 +57,7 @@
 #include <Gui/BitmapFactory.h>
 #include <Gui/Application.h>
 
-void PrintInitHelp(void);
+void PrintInitHelp();
 
 const char sBanner[] = "\xc2\xa9 Juergen Riegel, Werner Mayer, Yorik van Havre and others 2001-2021\n"\
 "FreeCAD is free and open-source software licensed under the terms of LGPL2+ license.\n"\
