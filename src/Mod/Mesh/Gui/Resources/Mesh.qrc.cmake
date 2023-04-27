b01a4750890ac24052f7bb28a56c6e7bdab31c4b
wmayer <wmayer@users.sourceforge.net>
2023-04-27 19:09:21 +0200

    Mesh: create QM files from TS files

---
+++ src/Mod/Mesh/Gui/Resources/Mesh.qrc.cmake
@@ -0,0 +1,91 @@
+<RCC>
+    <qresource>
+        <file alias="icons/Mesh_AddFacet.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_AddFacet.svg</file>
+        <file alias="icons/Mesh_BoundingBox.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_BoundingBox.svg</file>
+        <file alias="icons/Mesh_BuildRegularSolid.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_BuildRegularSolid.svg</file>
+        <file alias="icons/Mesh_CrossSections.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_CrossSections.svg</file>
+        <file alias="icons/Mesh_CurvatureInfo.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_CurvatureInfo.svg</file>
+        <file alias="icons/Mesh_Decimating.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Decimating.svg</file>
+        <file alias="icons/Mesh_Difference.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Difference.svg</file>
+        <file alias="icons/Mesh_EvaluateFacet.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_EvaluateFacet.svg</file>
+        <file alias="icons/Mesh_EvaluateSolid.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_EvaluateSolid.svg</file>
+        <file alias="icons/Mesh_Evaluation.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Evaluation.svg</file>
+        <file alias="icons/Mesh_Export.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Export.svg</file>
+        <file alias="icons/Mesh_FillInteractiveHole.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_FillInteractiveHole.svg</file>
+        <file alias="icons/Mesh_FillupHoles.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_FillupHoles.svg</file>
+        <file alias="icons/Mesh_FlipNormals.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_FlipNormals.svg</file>
+        <file alias="icons/Mesh_FromPartShape.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_FromPartShape.svg</file>
+        <file alias="icons/Mesh_HarmonizeNormals.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_HarmonizeNormals.svg</file>
+        <file alias="icons/Mesh_Import.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Import.svg</file>
+        <file alias="icons/Mesh_Intersection.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Intersection.svg</file>
+        <file alias="icons/Mesh_Merge.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Merge.svg</file>
+        <file alias="icons/Mesh_Pipette.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Pipette.svg</file>
+        <file alias="icons/Mesh_PolyCut.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_PolyCut.svg</file>
+        <file alias="icons/Mesh_PolyTrim.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_PolyTrim.svg</file>
+        <file alias="icons/Mesh_RemeshGmsh.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_RemeshGmsh.svg</file>
+        <file alias="icons/Mesh_RemoveCompByHand.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_RemoveCompByHand.svg</file>
+        <file alias="icons/Mesh_RemoveComponents.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_RemoveComponents.svg</file>
+        <file alias="icons/Mesh_Scale.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Scale.svg</file>
+        <file alias="icons/Mesh_SectionByPlane.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_SectionByPlane.svg</file>
+        <file alias="icons/Mesh_SegmentationBestFit.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_SegmentationBestFit.svg</file>
+        <file alias="icons/Mesh_Segmentation.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Segmentation.svg</file>
+        <file alias="icons/Mesh_Smoothing.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Smoothing.svg</file>
+        <file alias="icons/Mesh_SplitComponents.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_SplitComponents.svg</file>
+        <file alias="icons/Mesh_Tree.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Tree.svg</file>
+        <file alias="icons/Mesh_Tree_Curvature_Plot.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Tree_Curvature_Plot.svg</file>
+        <file alias="icons/Mesh_TrimByPlane.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_TrimByPlane.svg</file>
+        <file alias="icons/Mesh_Union.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_Union.svg</file>
+        <file alias="icons/Mesh_VertexCurvature.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/Mesh_VertexCurvature.svg</file>
+        <file alias="icons/MeshWorkbench.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/MeshWorkbench.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Cone.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Cone.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Cube.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Cube.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Cylinder.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Cylinder.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Ellipsoid.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Ellipsoid.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Sphere.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Sphere.svg</file>
+        <file alias="icons/RegularSolids/Mesh_Torus.svg">${CMAKE_CURRENT_SOURCE_DIR}/Resources/icons/RegularSolids/Mesh_Torus.svg</file>
+    </qresource>
+    <qresource>
+        <file>translations/Mesh_af.qm</file>
+        <file>translations/Mesh_de.qm</file>
+        <file>translations/Mesh_fi.qm</file>
+        <file>translations/Mesh_fr.qm</file>
+        <file>translations/Mesh_hr.qm</file>
+        <file>translations/Mesh_it.qm</file>
+        <file>translations/Mesh_nl.qm</file>
+        <file>translations/Mesh_no.qm</file>
+        <file>translations/Mesh_pl.qm</file>
+        <file>translations/Mesh_ru.qm</file>
+        <file>translations/Mesh_uk.qm</file>
+        <file>translations/Mesh_tr.qm</file>
+        <file>translations/Mesh_sv-SE.qm</file>
+        <file>translations/Mesh_zh-TW.qm</file>
+        <file>translations/Mesh_pt-BR.qm</file>
+        <file>translations/Mesh_cs.qm</file>
+        <file>translations/Mesh_sk.qm</file>
+        <file>translations/Mesh_es-ES.qm</file>
+        <file>translations/Mesh_zh-CN.qm</file>
+        <file>translations/Mesh_ja.qm</file>
+        <file>translations/Mesh_ro.qm</file>
+        <file>translations/Mesh_hu.qm</file>
+        <file>translations/Mesh_pt-PT.qm</file>
+        <file>translations/Mesh_sr.qm</file>
+        <file>translations/Mesh_el.qm</file>
+        <file>translations/Mesh_sl.qm</file>
+        <file>translations/Mesh_eu.qm</file>
+        <file>translations/Mesh_ca.qm</file>
+        <file>translations/Mesh_gl.qm</file>
+        <file>translations/Mesh_kab.qm</file>
+        <file>translations/Mesh_ko.qm</file>
+        <file>translations/Mesh_fil.qm</file>
+        <file>translations/Mesh_id.qm</file>
+        <file>translations/Mesh_lt.qm</file>
+        <file>translations/Mesh_val-ES.qm</file>
+        <file>translations/Mesh_ar.qm</file>
+        <file>translations/Mesh_vi.qm</file>
+        <file>translations/Mesh_es-AR.qm</file>
+        <file>translations/Mesh_bg.qm</file>
+        <file>translations/Mesh_ka.qm</file>
+        <file>translations/Mesh_sr-CS.qm</file>
+        <file>translations/Mesh_be.qm</file>
+    </qresource>
+</RCC>
