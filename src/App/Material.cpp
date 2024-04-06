1f7de74848a5da5641b70b016904a136f344e0fd
wmayer <wmayer@users.sourceforge.net>
2024-04-06 21:01:17 +0200

    App: restore previous value of specular color for default material

--- src/App/Material.cpp
+++ src/App/Material.cpp
@@ -328,7 +328,7 @@ void Material::setType(const MaterialType MatType)
     default:
         ambientColor.set(0.3333f, 0.3333f, 0.3333f);
         diffuseColor .set(0.8000f,0.8000f,0.9000f);
-        specularColor.set(0.5333f, 0.5333f, 0.5333f);
+        specularColor.set(0.0000f,0.0000f,0.0000f);
         emissiveColor.set(0.0000f,0.0000f,0.0000f);
         shininess = 0.9000f;
         transparency = 0.0000f;
