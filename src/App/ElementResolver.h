4c7bb91daa0972052f504399d27b739ab78f8876
wmayer <wmayer@users.sourceforge.net>
2024-07-04 18:19:12 +0200

    App: Add convenience class ElementResolver to resolve sub-elements of geometric features

---
+++ src/App/ElementResolver.h
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/***************************************************************************
+ *   Copyright (c) 2024 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *                                                                         *
+ *   This file is part of FreeCAD.                                         *
+ *                                                                         *
+ *   FreeCAD is free software: you can redistribute it and/or modify it    *
+ *   under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   FreeCAD is distributed in the hope that it will be useful, but        *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with FreeCAD. If not, see                               *
+ *   <https://www.gnu.org/licenses/>.                                      *
+ *                                                                         *
+ **************************************************************************/
+
+#ifndef APP_ELEMENTRESOLVER_H
+#define APP_ELEMENTRESOLVER_H
+
+#include <string>
+#include <FCGlobal.h>
+
+namespace App
+{
+
+class DocumentObject;
+class AppExport ElementResolver
+{
+public:
+    ElementResolver(DocumentObject* obj, const std::string& name);
+    DocumentObject* getObject() const;
+    std::string getElementName() const;
+
+private:
+    void perform();
+
+private:
+    DocumentObject* object {};
+    std::string element;
+};
+
+}
+
+#endif // APP_ELEMENTRESOLVER_H
