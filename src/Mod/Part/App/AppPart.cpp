df7a7f8d918bd0e6429913d41a536317242f4279
wmayer <wmayer@freecad.org>
2025-01-06 22:20:56 +0100

    Part: Implement extension to add physical properties at runtime

--- src/Mod/Part/App/AppPart.cpp
+++ src/Mod/Part/App/AppPart.cpp
@@ -128,6 +128,7 @@
 #include "TopoShapeWirePy.h"
 #include "ToroidPy.h"
 #include "OCCError.h"
+#include "PhysicalExtension.h"
 #include "PrismExtension.h"
 #include "PropertyGeometryList.h"
 #include "PropertyTopoShapeList.h"
@@ -435,7 +436,9 @@ PyMOD_INIT_FUNC(Part)
     Part::AttachExtension       ::init();
     Part::AttachExtensionPython ::init();
 
-    Part::PrismExtension        ::init();
+    Part::PhysicalExtension      ::init();
+    Part::PhysicalExtensionPython::init();
+    Part::PrismExtension         ::init();
 
     Part::Feature               ::init();
     Part::FeatureExt            ::init();
