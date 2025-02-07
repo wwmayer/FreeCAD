cd0942674ecf77ecfd123fafba28c3b86b6e82bc
tritao <joao@tritao.eu>
2025-02-07 22:46:35 +0000

    Base: Standardize on Py::Long type for Python bindings.

--- src/Mod/Part/App/AppPartPy.cpp
+++ src/Mod/Part/App/AppPartPy.cpp
@@ -1053,7 +1053,7 @@ private:
                           if (!PyLong_Check(value)) {
                               throw Py::ValueError(err);
                           }
-                          int order = Py::Int(value);
+                          int order = Py::Long(value);
                           params.orders[s] = static_cast<TopoShape::Continuity>(order);
                           return;
                       });
@@ -1130,7 +1130,7 @@ private:
                           if (!PyLong_Check(value)) {
                               throw Py::ValueError(err);
                           }
-                          int order = Py::Int(value);
+                          int order = Py::Long(value);
                           params.orders[s] = static_cast<TopoShape::Continuity>(order);
                           return;
                       });
