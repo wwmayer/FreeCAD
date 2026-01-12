6f7c7b2028c8d4f992416dcca1cdf2ab26221d0d
wwmayer <wmayer@freecad.org>
2026-01-12 14:17:05 +0100

    Gui: Support of 'SelectAll' command

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -1290,20 +1290,28 @@ StdCmdSelectAll::StdCmdSelectAll()
     sWhatsThis    = "Std_SelectAll";
     sStatusTip    = QT_TR_NOOP("Select all");
     sPixmap       = "edit-select-all";
-    //sAccel        = "Ctrl+A"; // supersedes shortcuts for text edits
+    sAccel        = keySequenceToAccel(QKeySequence::SelectAll);
+    eType         = AlterSelection;
 }
 
 void StdCmdSelectAll::activated(int iMsg)
 {
     Q_UNUSED(iMsg);
-    SelectionSingleton& rSel = Selection();
-    App::Document* doc = App::GetApplication().getActiveDocument();
-    std::vector<App::DocumentObject*> objs = doc->getObjectsOfType(App::DocumentObject::getClassTypeId());
-    rSel.setSelection(doc->getName(), objs);
+    bool done = getGuiApplication()->sendMsgToFocusView("SelectAll");
+    if (!done) {
+        SelectionSingleton& rSel = Selection();
+        App::Document* doc = App::GetApplication().getActiveDocument();
+        std::vector<App::DocumentObject*> objs = doc->getObjectsOfType(App::DocumentObject::getClassTypeId());
+        rSel.setSelection(doc->getName(), objs);
+    }
 }
 
 bool StdCmdSelectAll::isActive()
 {
+    if (getGuiApplication()->sendHasMsgToFocusView("SelectAll")) {
+        return true;
+    }
+
     return App::GetApplication().getActiveDocument() != nullptr;
 }
 
