8ef6107b37f8ce6065efd7be0b513a70b053ef2d
wwmayer <wmayer@freecad.org>
2026-02-02 10:22:33 +0100

    RE: Fix B-spline curve fitting
    1. Fix curve fitting when setting parametrization type
    2. Improve German translation

--- src/Mod/ReverseEngineering/Gui/FitBSplineCurve.h
+++ src/Mod/ReverseEngineering/Gui/FitBSplineCurve.h
@@ -45,6 +45,8 @@ protected:
     void changeEvent(QEvent* e) override;
 
 private:
+    void setParametrizationTypes();
+    QString getCurrentParametrizationType() const;
     void toggleParametrizationType(bool on);
     void toggleSmoothing(bool on);
     void tryAccept();
